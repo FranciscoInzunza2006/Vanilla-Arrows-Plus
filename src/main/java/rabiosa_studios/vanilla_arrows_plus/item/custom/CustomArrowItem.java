f31e28415812e0acd3a94fe3e0e12a25bd02597c
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-08-28 14:21:56 -0700

    doc

--- src/main/java/rabiosa_studios/vanilla_arrows_plus/item/custom/CustomArrowItem.java
+++ src/main/java/rabiosa_studios/vanilla_arrows_plus/item/custom/CustomArrowItem.java
@@ -25,6 +25,8 @@ public class CustomArrowItem extends ArrowItem {
      * @param divergence  Vanilla divergence is {@value VANILLA_DIVERGENCE}
      * @param charge_time Vanilla charge time is {@value VANILLA_CHARGE_TIME}, this value is in seconds.
      *                    <p>Note: Since the formula for the damage of the arrow is damage * speed, increasing the speed a lot and lowering the damage only a bit will end up with a arrow that does way more damage than planned. This function fixes it automatically.</p>
+     *                    <p>Crossbows have a {@value VANILLA_CROSSBOW_SPEED_MULTIPLIER} speed multiplier</p>
+     *                    <p>Divergence doesn't affect crossbows</p>
      */
     public CustomArrowItem(Item.Settings settings, double max_damage, float speed, float divergence, float charge_time) {
         super(settings);
