407c1240f2e06f4d83bfe34e271ec012dcd4ace4
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-08-29 14:23:21 -0700

    Modified Mixin so it can access to the user of the crossbow

--- src/main/java/rabiosa_studios/vanilla_arrows_plus/mixin/CrossbowItemMixin.java
+++ src/main/java/rabiosa_studios/vanilla_arrows_plus/mixin/CrossbowItemMixin.java
@@ -1,16 +1,17 @@
 package rabiosa_studios.vanilla_arrows_plus.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.component.type.ChargedProjectilesComponent;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import rabiosa_studios.vanilla_arrows_plus.item.custom.CustomArrowItem;
 
 @Mixin(CrossbowItem.class)
@@ -21,11 +22,12 @@ public abstract class CrossbowItemMixin {
                 (user.getProjectileType(crossbow).getItem() instanceof CustomArrowItem arrow ? arrow.getChargeTime(user) : 1.0f);
     }
 
-    @Inject(method = "getSpeed", at = @At("RETURN"), cancellable = true)
-    private static void applyArrowSpeed(ChargedProjectilesComponent stack, CallbackInfoReturnable<Float> cir) {
-        // TODO: Move mixins up so it can get the user object
+    @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;getSpeed(Lnet/minecraft/component/type/ChargedProjectilesComponent;)F"))
+    private static float applyArrowSpeed(ChargedProjectilesComponent stack, Operation<Float> original, @Local(argsOnly = true)PlayerEntity user) {
         if (stack.getProjectiles().getFirst().getItem() instanceof CustomArrowItem arrow) {
-            cir.setReturnValue(CustomArrowItem.VANILLA_CROSSBOW_SPEED_MULTIPLIER * arrow.getSpeed(null));
+            return CustomArrowItem.VANILLA_CROSSBOW_SPEED_MULTIPLIER * arrow.getSpeed(user);
         }
+
+        return original.call(stack);
     }
 }
